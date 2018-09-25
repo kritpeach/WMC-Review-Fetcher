f63a186212f7542919a0f1755c692f4f753540af
Krit Chernkwansri <5831305003@lamduan.mfu.ac.th>
2018-09-25 23:15:48 +0700

    Screen capturing

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ const googleMapReview = async (googlUrl) => {
     const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
     const page = await browser.newPage()
     await page.goto(googlUrl)
+    await page.screenshot({path: `/var/www/html/wp-content/uploads/wmc-review/${googlUrl.split("/")[4]}.png`});
     const rating = await page.evaluate(() => parseFloat(document.querySelector(".section-star-display").textContent))
     const reviewCount = await page.evaluate(() => parseInt(document.querySelector(".section-rating-line .widget-pane-link").textContent.replace(/\D/g, '')))
     await browser.close();
